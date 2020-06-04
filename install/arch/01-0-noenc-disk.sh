af1c822fac3939379ec7a6e98477e2faba3088e4
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2020-06-04 22:07:37 +0400

    arch-install: add disk partition configs.

--- install/arch/01-0-noenc-disk.sh
+++ install/arch/01-0-noenc-disk.sh
@@ -2,15 +2,32 @@
 
 source ./configs/no-enc-disk.sh
 
-echo "--- DISK SETUP ---"
-
-_DISK=$1
-
-if [[ ! -b "$_DISK" ]]; then
-  echo -n " -- Choose disk: "
-  read _DISK
+if [[ "$1" == "" || "$2" == "" ]]; then
+  echo "Usage: ./01-0-noenc-disk.sh HARDWARE_DEVICE CONFIGURATION_FILE"
+  echo "Usage example: ./01-0-noenc-disk.sh /dev/nvme0n1 thinkpad-tb"
+  echo "list of available configurations:"
+  ls ./configs/devices
+  exit 1
 fi
 
+echo "--- DISK SETUP ---"
+
+_CONFIG=$2
+
+if [[ ! -f "$_CONFIG" ]]; then
+  echo "Could not find configuration file $_CONFIG"
+  echo "list of available configurations:"
+  ls ./configs/devices
+  exit 1
+fi
+
+source $_CONFIG
+if [[ "$CFG_CREATE_DISK" == "" ]]; then
+  echo "CFG_CREATE_DISK variable is not defined in the config file $_CONFIG"
+  exit 1
+fi
+
+_DISK=$1
 if [[ ! -b $_DISK ]]; then
   echo "Disk '$_DISK' is not available, aborting..."
   exit 1
@@ -39,3 +56,5 @@ fi
 
 echo "Deleteing partition table..."
 sfdisk --delete $_DISK
+
+fdisk --wipe=always $_DISK < $CFG_CREATE_DISK
---
+++ install/arch/configs/devices/thinkpad-tb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Thinkpad has 1TB SSD.
+# nvme0n1p1 - EFI.
+# nvme0n1p2 - /boot - 1GB
+# nvme0n1p3 - SWAP - 20GB
+# nvme0n1p4 - / - 100GB
+# nvme0n1p5 - /home - 150 GB
+# Rest is unallocated for now.
