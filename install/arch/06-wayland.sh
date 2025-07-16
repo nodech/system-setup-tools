5efeffbba8981dcb1d418bb7a3b94b7d18cf48d8
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2025-07-16 12:48:02 +0400

    pkg: update system package list.

--- install/arch/06-wayland.sh
+++ install/arch/06-wayland.sh
@@ -5,30 +5,46 @@ source $__DIRECTORY/configs/configs.sh
 
 echo "--- Wayland and stuff ---"
 
-pacman -S sway \
-  alsa-utils \
-  alacritty \
-  rofi \
+pacman -S alacritty \
+  bat \
+  ccache \
   dunst \
+  feh \
   firefox \
-  ttf-dejavu \
+  fprintd \
   fzf \
-  ripgrep \
-  inetutils \
-  rustup \
-  os-prober \
-  inetutils \
+  grim \
   htop \
+  inetutils \
+  inetutils \
+  lsd \
   nmap \
   openssh \
-  rsync \
+  os-prober \
+  otf-font-awesome \
+  pavucontrol \
+  pipewire \
+  pipewire-alsa \
+  pipewire-audio \
+  pipewire-pulse \
   ranger \
-  feh
-
-# Check the sway utils
-# pacman -S waybar \
-#   swaybg \
-#   swaylock \
-#   swayidle \
-#   wl-clipboard \
-#   wdisplays \
+  ripgrep \
+  rofi \
+  rsync \
+  rustup \
+  sccache \
+  slurp \
+  sway \
+  swaybg \
+  swayidle \
+  swaylock \
+  ttf-dejavu \
+  udisks2 \
+  usbutils \
+  vulkan-intel \
+  waybar \
+  wayland-utils \
+  wireplumber \
+  wl-clipboard \
+  yubikey-personalization-gui \
+  zoxide \
--- install/arch/devices/thinkpad-gen13/04-system-setup.sh
+++ install/arch/devices/thinkpad-gen13/04-system-setup.sh
@@ -5,17 +5,16 @@ __DIRECTORY=`dirname $0`
 echo "--- Thinkpad (13 gen) system setup ---"
 
 echo " --- Install firmware updates ---"
-# echo " --- Refreshing devices..."
-# fwupdmgr refresh
+echo " --- Refreshing devices..."
+fwupdmgr refresh
 
-# echo " --- Update firmware (Interactive)..."
-# fwupdmgr update
+echo " --- Update firmware (Interactive)..."
+fwupdmgr update
 
 echo " --- Check if S3 is enabled..."
 dmesg | grep -i "acpi: (support"
 
-# echo " --- Fixing Throttling issue "
-# systemctl enable --now lenovo_fix.service
+pacman -S sof-firmware
 
 echo " --- Copying udev rules... "
 cp -v $__DIRECTORY/etc/udev/rules.d/* /etc/udev/rules.d/
